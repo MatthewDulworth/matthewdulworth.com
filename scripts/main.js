ea9da64d05b69c8a4b3d2f75fc7c593c59539792
Matthew Dulworth <mhdulworth@gmail.com>
2020-06-14 12:53:28 -0500

    Added current tab change on scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,48 @@
 // ------- elements ------- //
 let scrollBtns = document.querySelectorAll(".trigger div");
+let scrollBoxes = document.querySelectorAll(".scroll-box");
+let scrollPane = document.querySelector("#scroll-pane");
 
-scrollBtns.forEach(btn => btn.addEventListener('click', () => scroll(btn)));
 
-function scroll(button){
-   console.log(`yeet: ${button.textContent}`);
-   setCurrentTab(button);
+// ------- event listeners ------- //
+/**
+ * Listens for the scroll event in 
+ * 
+ * @listens scroll 
+ */
+scrollPane.addEventListener('scroll', () => {
+   scrollBoxes.forEach((box, index) => {
+      if (inView(box)) {
+         setCurrentTab(scrollBtns[index]);
+      }
+   });
+});
+
+
+// ------- functions ------- //
+/**
+ * Checks to see if the passed element is completely in the viewport.
+ * 
+ * @param {Element} element An html element. 
+ * @return {boolean} True if the element is in the viewport. 
+ */
+function inView(element) {
+   let bounds = element.getBoundingClientRect();
+   let window_w = window.innerWidth || document.documentElement.clientWidth;
+   let window_h = window.innerHeight || document.documentElement.clientHeight;
+   // If bounding rect is entirely within the viewport, return true. 
+   return (bounds.top >= 0 && bounds.left >= 0 && bounds.right <= window_w && bounds.bottom <= window_h);
 }
 
-function setCurrentTab(button){
-   if(button.id != "current-tab"){
-      for(btn of scrollBtns) {
-         if (btn.id === "current-tab"){
+/**
+ * Removes sets the passed button to the current tab.
+ * 
+ * @param {Element} button A scroll button. 
+ */
+function setCurrentTab(button) {
+   if (button.id != "current-tab") {
+      for (btn of scrollBtns) {
+         if (btn.id === "current-tab") {
             btn.id = "";
          }
       }
